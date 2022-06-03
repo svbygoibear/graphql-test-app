7ad97414b7b448823737f99732db2192de154fa4
Simone van Buuren <svanbuuren@qorusdocs.com>
2022-06-03 13:28:40 +0200

    Fixed up the controller for comparison

--- graphql-test-app.hotchocolate/README.md
+++ graphql-test-app.hotchocolate/README.md
@@ -1,3 +1,7 @@
 ##
 
+[Hot Chocolate Getting Started](https://chillicream.com/docs/hotchocolate/get-started)
+
+##
+
 [Local Host](https://localhost:44370/graphql/)
--- graphql-test-app.hotchocolate/graphql-test-app.hotchocolate/Controllers/WeatherForecastController.cs
+++ graphql-test-app.hotchocolate/graphql-test-app.hotchocolate/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using graphql_test_app.hotchocolate.IService;
 using graphql_test_app.hotchocolate.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,16 +9,18 @@ namespace graphql_test_app.hotchocolate.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly ILogger<WeatherForecastController> _logger;
+        private IWeatherForecastService _weatherForecastService = null;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService)
         {
+            _weatherForecastService = weatherForecastService;
             _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return null;
+            return _weatherForecastService.GetForecasts();
         }
     }
 }
